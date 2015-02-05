91f245a8f977113b77445669a47e6d9233ee0b36
Jeffrey Humphrey <humphrjm@rose-hulman.edu>
2015-02-05 15:32:14 -0500

    SE update and SE test
    SE Test is BROKEN, someone please look at, I cannot find the error

--- SignExtend.v
+++ SignExtend.v
@@ -18,29 +18,43 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module SignExtend(
-    input in[16:0],
-    input CLK,
-    input reset,
-	 input imSlct,
-    output out
+module SignExtend(in,
+    CLK,
+    reset,
+	 imSlct,
+    out
     );
+	 input imSlct;
+	 input CLK, reset;
+	 input [15:0] in;
+	 output reg [15:0] out;
 	 reg i;
+	 reg temp[15:0];
 	 reg currentstate;
 	 reg nextstate;
 	 always @ (reset) begin out=0; end
 	 always @ (posedge CLK) begin
+		for(i=0;i<10;i=i+1) 
+		begin
+			temp[i]=in[i];
+		end
 		if (imSlct==0) begin
+			temp[10]=in[10];
+			temp[11]=in[11];
 			for(i=12;i<16;i=i+1)
 			begin
-				in[i]=in[11];
+				temp[i]=in[11];
 			end
 		end
 		else begin
 			for(i=10;i<16;i=i+1)
 			begin
-				in[i]=in[9];
+				temp[i]=in[9];
 			end
+		end
+		for(i=0;i<16;i=i+1)
+		begin
+			out[i] = temp[i];
 		end
 	 end
 endmodule
